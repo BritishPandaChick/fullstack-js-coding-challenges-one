2bd1161bde710245fb790cd7c7f44bfe69516b39
BritishPandaChick <sbartley1080@gmail.com>
2017-12-20 23:50:05 -0500

    Add do-politely challenge. Update do-play-theremin code.

--- ask-politely.js
+++ ask-politely.js
@@ -1,1 +1,18 @@
-ÿþ
+function askPolitely(sentence) {
+  if (sentence[sentence.length-1] === "?") {
+    if (sentence.slice(-7) === "please?") {
+      return sentence;
+    } else {
+      return sentence.slice(0, -1) + " please?";
+    }
+  } else {
+    return sentence;
+  }
+}
+
+askPolitely("May I borrown your pencil?");
+askPolitely("May I ask a question please?");
+askPolitely("My name is Grace Hooper.");
+
+//"please".length;
+//"May I ask a question please?".slice(-7) === "please?";
--- do-play-theremin.js
+++ do-play-theremin.js
@@ -1,5 +1,5 @@
 function doYouPlayTheremin(name) {
-  if (name[0].toLowerCase === "s") {
+  if (name[0].toLowerCase === "s" || name[0] === "S") {
     return name + " plays the Theremin!";
   } else {
     return name + " does not play the Theremin!";
@@ -8,3 +8,12 @@ function doYouPlayTheremin(name) {
 
 doYouPlayTheremin("Griffin");
 doYouPlayTheremin("Scott");
+
+//Second alternative
+function doYouPlayTheremin(name) {
+  if (name[0].toUpperCase === "S" || name[0] === "s") {
+    return name + " plays the Theremin!";
+  } else {
+    return name + " does not play the Theremin!";
+  }
+}
